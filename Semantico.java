36d03067497cae653d09ee5cc10e1194039bd6f0
Guilherme Mohr <72572592+GuilhermeMohr@users.noreply.github.com>
2025-10-06 01:27:52 +0000

    Alterações finais

--- Semantico.java
+++ Semantico.java
@@ -16,14 +16,14 @@ public class Semantico implements Constants
 
         switch(action) {
             case 1: //Binario
-                System.out.println(Integer.parseInt(token.getLexeme(), 2));
+                System.out.println("Armazenou: " + Integer.parseInt(token.getLexeme(), 2));
                 binaries.push(Integer.parseInt(token.getLexeme(), 2));
                 break;
             case 2: //Variável
                 binaries.push(map.get(token.getLexeme()));
                 break;
             case 3: //Imprimir
-                System.out.println(Integer.toBinaryString(binaries.pop()));
+                System.out.println("Impressão: " + Integer.toBinaryString(binaries.pop()));
                 break;
             case 4: //Atribuir a variável
                 var = token.getLexeme();
@@ -31,43 +31,43 @@ public class Semantico implements Constants
             case 5: //Variável
                 Integer ok = binaries.pop();
                 map.put(var, ok);
-                System.out.println(var + " - " + ok);
+                System.out.println("Atribuicao de variavel: " + (var + " = " + ok));
                 break;
             case 6: //Soma
                 i = binaries.pop();
                 ii = binaries.pop();
-                System.out.println(i + ii);
+                System.out.println("Resultado soma: " + (i + ii));
                 binaries.push(i + ii);
                 break;
             case 7: //Subtração
                 ii = binaries.pop();
                 i = binaries.pop();
-                System.out.println(i - ii);
+                System.out.println("Resultado subtracao: " + (i - ii));
                 binaries.push(i - ii);
                 break;
             case 8: //Multiplicação
                 i = binaries.pop();
                 ii = binaries.pop();
-                System.out.println(i * ii);
+                System.out.println("Resultado multiplicacao: " + (i * ii));
                 binaries.push(i * ii);
                 break;
             case 9: //Divisão
                 ii = binaries.pop();
                 i = binaries.pop();
-                System.out.println(Math.round((i.floatValue() / ii.floatValue())));
+                System.out.println("Resultado divisao: " + (Math.round((i.floatValue() / ii.floatValue()))));
                 binaries.push(Math.round((i.floatValue() / ii.floatValue())));
                 break;
             case 10: //Exponenciação
                 ii = binaries.pop();
                 i = binaries.pop();
-                System.out.println(Math.pow(i.doubleValue(), ii.doubleValue()));
+                System.out.println("Resultado exponenciacao: " + (Math.pow(i.doubleValue(), ii.doubleValue())));
                 binaries.push((int) Math.pow(i.doubleValue(), ii.doubleValue()));
                 break;
             case(11): //Log
                 i = binaries.pop();
                 ii = binaries.pop();
                 
-                System.out.println(Math.log(i.doubleValue()) / Math.log(ii.doubleValue()));
+                System.out.println("Resultado log: " + (Math.log(i.doubleValue()) / Math.log(ii.doubleValue())));
                 binaries.push((int) Math.round(Math.log(i.doubleValue()) / Math.log(ii.doubleValue())));
                 break;
         }
